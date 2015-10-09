f2ffab48a659e092d95b19e13a8a867f6fc3a566
jaedm97 <jaedm97@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-09 18:14:46 +0000

    removed not necessary
    git-svn-id: https://plugins.svn.wordpress.org/woc-open-close/trunk@1262634 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($)
-{	
-	$(document).on('click', '.woc_btn_woocommerce_close', function()
-		{
-			var woc_off_message 	= $(this).attr('woc_off_message');
-			alert(woc_off_message);
-		}
-	)
-});
